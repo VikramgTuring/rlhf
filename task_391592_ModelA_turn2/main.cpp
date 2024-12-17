0a7ec5ac73c4409a8242bdafdedfec752abf365d
vikramgturing <vikram.g@turing.com>
2024-12-17 15:14:13 +0000

    task_391592_ModelA_turn2

--- task_391592_ModelA_turn2/main.cpp
+++ task_391592_ModelA_turn2/main.cpp
@@ -1,94 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-#include <algorithm>
 
-// Observer Interface
-class IObserver {
+class Observer {
 public:
-    virtual ~IObserver() = default;
-    virtual void onNotify() = 0;
+    virtual ~Observer() = default;
+    virtual void update() = 0;
 };
 
-// Subject Class
-class ISubject {
+class Subject {
 public:
-    virtual ~ISubject() = default;
-    virtual void attach(std::shared_ptr<IObserver> observer) = 0;
-    virtual void detach(std::shared_ptr<IObserver> observer) = 0;
-    virtual void notify() = 0;
-};
-
-// Button Class acting as Subject
-class Button : public ISubject {
-public:
-    void click() {
-        std::cout << "Button clicked!" << std::endl;
-        notify();
-    }
-
-    void attach(std::shared_ptr<IObserver> observer) override {
+    void attach(std::shared_ptr<Observer> observer) {
         observers.push_back(observer);
     }
 
-    void detach(std::shared_ptr<IObserver> observer) override {
+    void detach(std::shared_ptr<Observer> observer) {
         observers.erase(std::remove(observers.begin(), observers.end(), observer), observers.end());
     }
 
-    void notify() override {
+    void notifyAllObservers() {
         for (const auto& observer : observers) {
-            observer->onNotify();
+            observer->update();
         }
     }
 
 private:
-    std::vector<std::shared_ptr<IObserver>> observers;
+    std::vector<std::shared_ptr<Observer>> observers;
 };
 
-// Label Class acting as Observer
-class Label : public IObserver {
+class Subscriber : public Observer {
 public:
-    void onNotify() override {
-        std::cout << "Label updated because button clicked!" << std::endl;
+    void update() override {
+        std::cout << "Subscriber notified." << std::endl;
     }
 };
 
-// Legacy-style Label that directly handles button clicks
-class LegacyLabel {
-public:
-    void onButtonClicked() {
-        std::cout << "LegacyLabel updated because button clicked!" << std::endl;
-    }
-};
-
-// Adapter for the LegacyLabel to fit into the Observer Pattern
-class LegacyLabelAdapter : public IObserver {
-public:
-    LegacyLabelAdapter(LegacyLabel& legacyLabel) : legacyLabel(legacyLabel) {}
-
-    void onNotify() override {
-        legacyLabel.onButtonClicked();
-    }
-
-private:
-    LegacyLabel& legacyLabel;
-};
-
-// Main function demonstrating the integration
 int main() {
-    Button button;
-    
-    // New Label
-    auto newLabel = std::make_shared<Label>();
-    button.attach(newLabel);
-    
-    // Legacy Label
-    LegacyLabel legacyLabel;
-    auto legacyLabelAdapter = std::make_shared<LegacyLabelAdapter>(legacyLabel);
-    button.attach(legacyLabelAdapter);
-    
-    // Simulating button click
-    button.click();  // Both new and legacy labels will respond
+    Subject subject;
+
+    // Attach observers
+    auto observer1 = std::make_shared<Subscriber>();
+    auto observer2 = std::make_shared<Subscriber>();
+
+    subject.attach(observer1);
+    subject.attach(observer2);
+
+    subject.notifyAllObservers();
+
+    // Removing an observer
+    subject.detach(observer1);
+    subject.notifyAllObservers();
 
     return 0;
 }
